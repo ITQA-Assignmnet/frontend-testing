3ee7cafed49b522a07b6f020dbc99cf76f94a126
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-01-03 01:34:26 +0530

    about test case

---
+++ cypress/support/step_definitions/about.js
@@ -0,0 +1,27 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I am logged in as a standard user for visit about page", () => {
+    cy.visit('/');
+    cy.get('#user-name').type('standard_user'); 
+    cy.get('#password').type('secret_sauce');  
+    cy.get('#login-button').click();           
+    cy.url().should('include', '/inventory.html'); 
+});
+
+When("I click the menu icon for display the side bar", () => {
+    cy.get(".bm-burger-button").click(); 
+});
+
+Then("The sidebar should be display", () => {
+    cy.get(".bm-menu").should("be.visible"); 
+});
+
+When("I click the about button", () => {
+    cy.get("#about_sidebar_link").click(); 
+});
+
+Then("I should be navigated to the about page", () => {
+    cy.url().should("include", "https://saucelabs.com");
+      
+   
+});
